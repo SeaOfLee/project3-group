7fac3077be67cda1253875ad701e4acb1434a56b
Katie Kelleher <kt.kllhr@gmail.com>
2015-03-05 16:49:34 -0800

    attempt to add tests to sessions controller

---
+++ alumni_app/spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,22 @@
+# require 'rails_helper'
+
+# describe SessionsController do
+#   end
+
+# describe "GET #new" do
+#     before(:each) do
+#       get :new
+#     end
+
+#     it "should render the correct page" do
+#       expect(response).to render_template :new
+#     end
+
+#     it "should make a new Session" do
+#       expect(assigns(:session)).to be_a(Session)
+#     end
+
+#     it "is not persisted" do
+#      expect{Session.new}.to change(Session, :count).by(0)
+#    end
+#   end
