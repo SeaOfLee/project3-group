4e66037d9997c0a90af03fab589db965ec0b827e
Katie Kelleher <kt.kllhr@gmail.com>
2015-03-05 18:31:38 -0800

    add devise to project

--- alumni_app/app/models/user.rb
+++ alumni_app/app/models/user.rb
@@ -51,19 +51,3 @@ class DeviseCreateUsers < ActiveRecord::Migration
   end
 end
 
-# migration
-create_table(:users) do |t|
-  t.database_authenticatable :null => false
-  # t.confirmable
-  t.recoverable
-  t.rememberable
-  t.trackable
-  # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-
-  t.timestamps
-end
-
-add_index :users, :email,                :unique => true
-# add_index :users, :confirmation_token,   :unique => true
-add_index :users, :reset_password_token, :unique => true
-# add_index :users, :unlock_token,         :unique => true
