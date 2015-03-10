b494730a1d420816b37befd0cbc48648876c278c
laurenpawell <lauren.pawell@gmail.com>
2015-03-09 19:17:55 -0700

    Add User model tests

---
+++ alumni_app/spec/factories/user_factory.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+
+  # By default factory girl assumes the :cat symbol refers to a Cat class
+  #it believes you likely defined in app/models/cat.rb
+  factory :user do
+    first_name "bob"
+    last_name  "siamese"
+    email "bob@siamese.com"
+
+    initialize_with { new(first_name, last_name, email) }
+  end
+end
--- alumni_app/spec/models/user_spec.rb
+++ alumni_app/spec/models/user_spec.rb
@@ -1,32 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  before(:each)  do
-    @valid_user = User.create(
-      :first_name => "Lee",
-      :last_name => "Person",
-      :email => "lee@lee.com",
-      :password => "123"
-    )
+  # before(:each)  do
+  #   @valid_user = User.create(
+  #     :first_name => "Lee",
+  #     :last_name => "Person",
+  #     :email => "lee@lee.com",
+  #     :password => "123"
+  #   )
+  #
+  #   @invalid_user = User.create(
+  #     :first_name => " ",
+  #     :last_name => "Person",
+  #     :email => "person@person.com",
+  #     :password => "123"
+  #   )
+  # end
 
-    @invalid_user = User.create(
-      :first_name => " ",
-      :last_name => "Person",
-      :email => "person@person.com",
-      :password => "123"
-    )
+  let(:user) { FactoryGirl.create :user }
+
+  describe "validates presence of  first_name" do
+    it { should validate_presence_of(:first_name) }
   end
 
-  it "is invalid without a first name" 
+  describe "validates presence of last_name" do
+    it { should validate_presence_of(:last_name) }
+  end
 
-  it "is invalid without a last name"
+  describe "validates presence of email" do
+    it { should validate_presence_of(:email) }
+  end
 
-  it "is invalid without an email"
+  describe "validates presence of password" do
+    it { should validate_presence_of(:password) }
+  end
 
-  it "is invalid with a duplicate email"
+  describe "validates uniqueness_of_email" do
+    it { should validate_uniqueness_of(:email) }
+  end
 
-  
-
-
+  # describe "validates that users have many comments through posts" do
+  #   it { expect(user).to have_many(:comments).through(:posts) }
+  # end
 
 end
