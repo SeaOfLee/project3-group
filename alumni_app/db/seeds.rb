43f44c355e1c511d003e5aa431ac9a5784eeb448
SeaOfLee <l.richardson1@gmail.com>
2015-03-11 19:48:48 -0700

    Change seeds.rb, add location and remove postal_code and country

--- alumni_app/db/seeds.rb
+++ alumni_app/db/seeds.rb
@@ -11,7 +11,7 @@
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     password: Faker::Internet.password,
-    postal_code: Faker::Address.postcode,
+    current_location: Faker::Address.postcode,
     campus: Faker::Address.city,
     bio: Faker::Hacker.say_something_smart,
     )
